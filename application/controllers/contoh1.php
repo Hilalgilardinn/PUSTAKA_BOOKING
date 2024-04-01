1b0cde591cd2b1f48784adff3a009c18d900971d
Hilalgilardinn <muhammadhilalgilardin@gmail.com>
2024-04-01 19:48:07 +0700

    pertemuan 3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,18 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Imam Nawawi
+        Saya tingga di daerah Ciputat
+        olah raga yang saya sukai adalah
+        Bulutangkis";
+    }
+
+    public function tes ()
+    {
+        echo "Hi Nama Saya Hilal " ;
+        echo "Umur Saya 20 TAHUN" ;
+    }
+}
